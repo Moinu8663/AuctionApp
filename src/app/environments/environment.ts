a7b363800ae1566309a52230a44d04a0eda550be
moinuddin sheikh <moinuddin8663@gmail.com>
2026-05-07 17:26:32 +0530

    update

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,8 +1,8 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'https://localhost:7155/api/'
-};
 // export const environment = {
 //   production: false,
-//   apiBaseUrl: 'https://auctionapi-habeewbfh6b5cpaj.centralindia-01.azurewebsites.net/api/'
+//   apiBaseUrl: 'https://localhost:7155/api/'
 // };
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://auctionapi-habeewbfh6b5cpaj.centralindia-01.azurewebsites.net/api/'
+};
